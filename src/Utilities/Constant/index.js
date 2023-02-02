c324341bc18e03746be71d6f3b78198d3fe11ca0
Nguyen Van Vinh <vanvinhqn7350@gmail.com>
2023-02-02 21:54:47 +0700

    create Location Slice with function find location, set and get location in storage

--- src/Utilities/Constant/index.js
+++ src/Utilities/Constant/index.js
@@ -1,5 +1,4 @@
-export const API_KEY = '3c8f65e4fcc3413883b90444232501';
-
+export const API_KEY = 'c4dfe9f907b5a12595aa4451b52e624f';
 export const LANGUAGE = {
     English : {
         lang : 'en'
