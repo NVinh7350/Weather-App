5ffcc17fd57e6c04b2622d7bb53b22c56030e432
Nguyen Van Vinh <vanvinhqn7350@gmail.com>
2023-02-08 15:27:23 +0700

    created Summary Card Component

--- src/Components/SummaryCard/index.js
+++ src/Components/SummaryCard/index.js
@@ -1,17 +1,42 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Colors, screenHeight, screenWidth } from '../../Utilities'
+import { useSelector } from 'react-redux'
+import { weatherForecastsListSelect } from '../../Redux/Feature/WeatherForecastSlice'
+import { unitSelect } from '../../Redux/Feature/SettingSlice'
 
-const SummaryCard = ({}) => {
-  return (
-    <View style={ styles.container }>
-        <View style={ styles.temperatureContainer }>
-            <Text style={ styles.numberText }>-66</Text>
-            <Text style={ styles.unitText }>ºC</Text>
+const SummaryCard = ({ location }) => {
+    const [currentWeather, setCurrentWeather] = useState({});
+
+    const unitTemperature = useSelector(unitSelect);
+    const weatherForecastsList = useSelector(weatherForecastsListSelect);
+
+    useEffect(() => {
+        setCurrentWeather(() => {
+            for (const iterator of weatherForecastsList) {
+                if (iterator?.location?.name == location?.name) {
+                    return iterator.weatherForecast.currentWeather;
+                }
+            }
+            return null;
+        })
+    }, [weatherForecastsList])
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.temperatureContainer}>
+                <Text style={styles.numberText}>
+                    {Math.round(currentWeather?.main?.temp)}
+                </Text>
+                <Text style={styles.unitText}>
+                    {`º${unitTemperature == 'metric' ? 'C' : 'F'}`}
+                </Text>
+            </View>
+            <Text style={styles.describeText}>
+                {Array.isArray(currentWeather?.weather) ? currentWeather?.weather[0]?.description : ''}
+            </Text>
         </View>
-        <Text style={ styles.describeText }>Patchy freezing drizzle possible</Text>
-    </View>
-  )
+    )
 }
 
 export default SummaryCard
@@ -20,29 +45,33 @@ const styles = StyleSheet.create({
     container: {
         height: screenHeight * 0.25,
         width: screenWidth * 0.6,
-        alignItems:'center',
+        alignItems: 'center',
     },
     temperatureContainer: {
         height: screenHeight * 0.17,
+        width: screenWidth * 0.6,
         flexDirection: 'row',
-        justifyContent:'center',
     },
     numberText: {
+        flex: 3,
         fontSize: 110,
-        textAlign: 'center',
+        textAlign: 'right',
         letterSpacing: -5,
-        color: Colors.WHITE
+        color: Colors.WHITE,
     },
-    unitText : {
+    unitText: {
+        flex: 1,
         fontSize: 20,
-        marginTop:30,
+        marginTop: 30,
         fontWeight: 'bold',
-        color: Colors.WHITE
+        textAlign: 'left',
+        color: Colors.WHITE,
+
     },
     describeText: {
         fontSize: 20,
         fontWeight: 'bold',
         textAlign: 'center',
-        color: Colors.WHITE
+        color: Colors.WHITE,
     }
 })
